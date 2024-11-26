f9e6ac4a61c2ee840f371dda99a0eb8baf7fea25
Patrick Dumdum <patrickedumdum@gmail.com>
2024-11-26 12:16:30 +0000

    added minimum functionallity for post page

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostsController {
@@ -23,7 +25,24 @@ public class PostsController {
         model.addAttribute("post", new Post());
         return "posts/index";
     }
-    
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView getPost(@PathVariable Long id){
+
+        ModelAndView modelAndView = new ModelAndView("posts/show");
+        Optional<Post> tryGetPost = repository.findById(id);
+
+        if (tryGetPost.isEmpty()){
+            //Error handle here later
+        }
+        Post post = tryGetPost.get();
+        modelAndView.addObject("post",post);
+
+        return modelAndView;
+
+
+
+    }
 
     @PostMapping("/posts")
     public RedirectView create(@ModelAttribute Post post) {
